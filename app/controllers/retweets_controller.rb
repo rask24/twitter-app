63f200ecee719d8e4f1e7837ddeb9bcdf1dd65c2
rask24 <rask0x18@gmail.com>
2023-03-06 20:32:38 +0900

    styling with tailwindcss

--- app/controllers/retweets_controller.rb
+++ app/controllers/retweets_controller.rb
@@ -2,9 +2,6 @@ class RetweetsController < ApplicationController
   def create
     if Retweet.create(user_id: retweet_params[:user_id], tweet_id: retweet_params[:tweet_id])
       if retweet_params[:from] == 'tweets_index'
-        p '+++++++++++++++++++++++++'
-        p retweet_params
-        p '+++++++++++++++++++++++++'
         redirect_to root_path
       elsif retweet_params[:from] == 'users_show'
         redirect_to user_path(retweet_params[:page_id])
