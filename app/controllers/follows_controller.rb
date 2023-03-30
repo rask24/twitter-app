cdcb7aad29053890e7bc789bac92cbadb7888124
rask24 <rask0x18@gmail.com>
2023-03-30 23:44:08 +0900

    implement feature tweet search

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -5,25 +5,26 @@ class FollowsController < ApplicationController
     @user = User.find(follow_params[:followee_id])
     Follow.create(follower_id: follow_params[:follower_id],
                   followee_id: follow_params[:followee_id])
-    render turbo_stream: [
-      turbo_stream.replace("follow_button_#{@user.id}", partial: 'users/user_follow_button'),
-      turbo_stream.replace(:follower_count, partial: 'users/user_follower_count')
-    ]
+    render_with_turbo_stream
   end
 
   def destroy
     follow_rel = Follow.find(follow_params[:id])
     @user = User.find(follow_rel.followee_id)
     follow_rel.destroy
-    render turbo_stream: [
-      turbo_stream.replace("follow_button_#{@user.id}", partial: 'users/user_follow_button'),
-      turbo_stream.replace(:follower_count, partial: 'users/user_follower_count')
-    ]
+    render_with_turbo_stream
   end
 
   private
 
   def follow_params
     params.permit(:follower_id, :followee_id, :id)
+  end
+
+  def render_with_turbo_stream
+    render turbo_stream: [
+      turbo_stream.replace("follow_button_#{@user.id}", partial: 'users/user_follow_button'),
+      turbo_stream.replace(:follower_count, partial: 'users/user_follower_count')
+    ] and return
   end
 end
--- app/controllers/retweets_controller.rb
+++ app/controllers/retweets_controller.rb
@@ -5,29 +5,30 @@ class RetweetsController < ApplicationController
     retweet = Retweet.new(user_id: current_user.id, tweet_id: retweet_params[:tweet_id])
     retweet.save
     tweet = Tweet.find(retweet_params[:tweet_id])
-    render turbo_stream: [
-      turbo_stream.replace_all(".retweet_button_#{retweet_params[:tweet_id]}",
-                                                  partial: 'share/tweet_item_retweet_button',
-                                                  locals: { tweet: tweet }),
-      turbo_stream.replace("retweet_button_detail_#{retweet_params[:tweet_id]}", partial: 'share/tweet_item_retweet_button_detail', locals: { tweet: tweet })
-    ]
+    render_with_turbo_stream(tweet)
   end
 
   def destroy
     retweet_to_del = Retweet.find(retweet_params[:id])
     retweet_to_del.destroy
     tweet = Tweet.find(retweet_params[:tweet_id])
-    render turbo_stream: [
-      turbo_stream.replace_all(".retweet_button_#{retweet_params[:tweet_id]}",
-                                                  partial: 'share/tweet_item_retweet_button',
-                                                  locals: { tweet: tweet }),
-      turbo_stream.replace("retweet_button_detail_#{retweet_params[:tweet_id]}", partial: 'share/tweet_item_retweet_button_detail', locals: { tweet: tweet })
-    ]
+    render_with_turbo_stream(tweet)
   end
 
   private
 
   def retweet_params
     params.permit(:id, :tweet_id)
+  end
+
+  def render_with_turbo_stream(tweet)
+    render turbo_stream: [
+      turbo_stream.replace_all(".retweet_button_#{retweet_params[:tweet_id]}",
+                                                  partial: 'share/tweet_item_retweet_button',
+                                                  locals: { tweet: }),
+      turbo_stream.replace("retweet_button_detail_#{retweet_params[:tweet_id]}",
+                           partial: 'share/tweet_item_retweet_button_detail',
+                           locals: { tweet: })
+    ]
   end
 end
